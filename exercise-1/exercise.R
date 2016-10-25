2e5e3c6ad4aabfe885e47e41da3306d60635cf94
Michael Freeman <michael.k.freeman@gmail.com>
2016-10-24 17:42:56 -0700

    Removed exercise answers

--- exercise-1/exercise.R
+++ exercise-1/exercise.R
@@ -21,19 +21,3 @@ BestGearsCyl <- functon(gears, cylinders) {
 
 # Get the best mpg car for 6 cylinder cars with 4 gears
 answer <- BestGearsCyl(6, 4)
-
-######## Fixed ######### ----------------------------------------------
-
-# Function to debug:
-# Given a number of forward gears and a number of cylinders, what is the 
-# Name of the car with the best mpg?
-BestGearsCyl <- function(gears, cylinders) {
-  ret <- vehicle %>%  
-    filter(gear == gears, cyl == cylinders) %>% 
-    filter(mpg== max(mpg)) %>% 
-    select(car)
-  return (ret)
-}
-
-# Get the best mpg car for 6 cylinder cars with 4 gears
-answer <- BestGearsCyl(4, 6)
--- exercise-2/exercise.R
+++ exercise-2/exercise.R
@@ -8,43 +8,18 @@ setwd('~/Documents/info-201/m10-apis/exercise-2/')
 
 
 # Write a function that allows you to specify an artist, and returns the top 10 tracks of that artist
-TopTrackSearch <- function(artist) {
-  # Search base
-  base <- 'https://api.spotify.com/v1/'
-  
-  # Search artist
-  artist.query <- paste0(base, 'search?', 'q=', artist, '&type=artist')
-  
-  # Artist info 
-  artist.info <- fromJSON(artist.query)
-  artist.id <- artist.info$artists$items$id[1]
-  
-  # Get albums
-  album.query <- paste0(base, 'artists/', artist.id, '/top-tracks?country=US')
-  top.tracks <- fromJSON(album.query)
-  return(top.tracks$tracks)
-}
+
+
 
 # What are the top 10 tracks by Nelly?
-top.nelly <- TopTrackSearch('Nelly')
 
-# Save top_nelly
-save(top.nelly, file="nelly_tracks.Rdata")
+
+
 
 ### Bonus ### 
 # Write a function that allows you to specify a search type (artist, album, etc.), and a string, 
 # that returns the album/artist/etc. page of interest
-SpotifySearch <- function(type, string) {
-  # Search base
-  base <- 'https://api.spotify.com/v1/'
-  
-  # Search artist
-  search <- paste0(base, 'search?', 'q=', string, '&type=', type)
-  
-  # Artist info 
-  info <- fromJSON(search)
-  return(info)
-}
+
 
 # Search albums with the word "Sermon"
-SpotifySearch('album', 'Sermon')
+
