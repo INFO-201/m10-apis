9f93ab6a5a4c1f72563081e705b33f2098e82495
lleontan <lleontan@gmail.com>
2016-10-27 13:39:45 -0700

    pushing all the shit

--- exercise-1/exercise.R
+++ exercise-1/exercise.R
@@ -4,6 +4,8 @@
 library(dplyr)
 
 # We'll be using the mtcars dataset -- adding rowname as a column
+#install.packages("mtcars")
+library(fueleconomy)
 vehicle <- add_rownames(mtcars, var='car')
 
 
@@ -12,12 +14,13 @@ vehicle <- add_rownames(mtcars, var='car')
 # Function to debug:
 # Given a number of forward gears and a number of cylinders, what is the 
 # Name of the car with the best mpg?
-BestGearsCyl <- functon(gears, cylinders) {
-  ret <- vehicles %>%  
+BestGearsCyl <- function(gears, cylinders) {
+  ret <- vehicle %>%  
         filter(gear == gears, cyl == cylinders) %>% 
-        filter(mgp = max(mpg)) %>% 
-        select(car)
+        filter(mpg == max(mpg)) %>% 
+        select(car) 
+  return(ret)
 }
 
 # Get the best mpg car for 6 cylinder cars with 4 gears
-answer <- BestGearsCyl(6, 4)
+answer <- BestGearsCyl(4, 6)
--- exercise-2/exercise.R
+++ exercise-2/exercise.R
@@ -4,13 +4,36 @@
 # This is a perfect situation in which writing a function will allow you to better structure your
 # code, and give a name to a repeated task.
 library(jsonlite)
-setwd('~/Documents/info-201/m10-apis/exercise-2/')
+#setwd('~/Documents/info-201/m10-apis/exercise-2/')
 
 
 # Write a function that allows you to specify an artist, and returns the top 10 tracks of that artist
+base.API.str <- 'https://api.spotify.com/v1/search?'
 
-
-
+GetArtistId<-function(artist.name){
+  # Base URL of API
+  search <- paste('q=',artist.name,collapse="",sep="")
+  type <- '&type=artist'
+  query.url <- paste0(base.API.str, search, type)
+  json.file <-fromJSON(query.url)
+  #names(adele$artists$items$id[1])
+  artist.id<-json.file$artists$items$id[1]
+  #json.file <- lapply(json.file, function(x) {
+   # x[sapply(x, is.null)] <- NA
+  #  unlist(x)
+  #})
+  #data<-do.call("rbind", json.file)
+  #names(json.file)
+  return(artist.id)
+}
+GetArtistTracks<-function(artist.name){
+  artist.id<-GetArtistId(artist.name)
+  tracks.query<-paste0(base.API.str,"artists/",artist.id,"/top-tracks?country=US")
+  tracks<-fromJSON(tracks.query)
+  return(tracks)
+}
+adele<-GetArtistTracks("Adele")
+#print(adele$artists$items[[1]])
 # What are the top 10 tracks by Nelly?
 
 
